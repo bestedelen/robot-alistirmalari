e4cc54687f66b3857ba55c4250ccee0c1230cd07
Beste Delen <130577307+bestedelen@users.noreply.github.com>
2023-08-05 14:37:15 +0300

    Add files via upload

---
+++ asal.py
@@ -0,0 +1,16 @@
+"""def listeyeCevir(*params):
+    liste=[]
+    for param in params:
+        liste.append(param)
+
+    return liste
+result=listeyeCevir("meraba",10,20,30)
+print(result)
+"""    
+
+def square(num):
+    return num**2
+result=square(2)
+
+print(result)
+
